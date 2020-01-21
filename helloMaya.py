0da0d8d533d75a3a9b38b37088f994b8929b3594
fakeljl <694118974@qq.com>
2020-01-21 12:34:40 -0500

    Final

--- helloMaya.py
+++ helloMaya.py
@@ -4,11 +4,16 @@ import sys
 import maya.OpenMayaMPx as OpenMayaMPx
 import maya.OpenMaya as OpenMaya
 
+# Import the Python wrappers for MEL commands
+import maya.cmds as cmds
+
 # The name of the command.
 kPluginCmdName = 'pyHelloMaya'
 
-# kNameFlagShort = "-n"
-# kNameFlagLong = "-name"
+# [in] shortName the string representing the short (< 4 character) version of the flag
+# [in] longName the string representing the int (> 3 character) version of the flag
+kNameFlagShort = "-nf"
+kNameFlagLong = "-name"
 kIdFlagShort = "-i"
 kIdFlagLong = "-myIdFlag"
 
@@ -24,26 +29,32 @@ class helloMayaCommand(OpenMayaMPx.MPxCommand):
     def doIt(self, args):
         ''' Command execution. '''
 
-        # We recommend parsing your arguments first.
-        self.parseArguments(args)
-
-        # Remove the following 'pass' keyword and replace it with the code you want to run.
-        pass
+        # Parsing your arguments first.
+        info = self.parseArguments(args)
+        window = cmds.window(title="Hello Maya Python", widthHeight=(250, 80),
+                             minimizeButton=False, maximizeButton=False)
+        cmds.columnLayout(columnAttach=('both', 50), rowSpacing=10, columnWidth=250)
+        cmds.text(label="Name:" + info[0])
+        cmds.text(label="ID:" + info[1])
+        cmds.button(label='OK', command=('cmds.deleteUI(\"' + window + '\", window=True)'))
+        cmds.showWindow(window)
 
     def parseArguments(self, args):
         # The following MArgParser object allows you to check if specific flags are set.
         argData = OpenMaya.MArgParser(self.syntax(), args)
 
-        # ... If there are more flags, process them here ...
-        # if argData.isFlagSet(kNameFlag):
-        #     flagParam = argData.flagArgumentString(kNameFlag, 0)
-        #     print kNameFlag + ": " + flagParam
+        inputName = ""
+        inputID = "0"
+
+        if argData.isFlagSet(kNameFlagShort):
+            flagParam = argData.flagArgumentString(kNameFlagShort, 0)
+            inputName = str(flagParam)
 
         if argData.isFlagSet(kIdFlagShort):
-            # In this case, we print the passed flags's three parameters, indexed from 0 to 2.
             flagParam = argData.flagArgumentInt(kIdFlagShort, 0)
-            print kIdFlagShort + ': ' + str(flagParam)
+            inputID = str(flagParam)
 
+        return [inputName, inputID]
 
 ##########################################################
 # Plug-in initialization.
@@ -57,7 +68,7 @@ def syntaxCreator():
     ''' Defines the argument and flag syntax for this command. '''
     syntax = OpenMaya.MSyntax()
 
-    # syntax.addFlag(kNameFlag, kNameFlag, OpenMaya.MSyntax.kString)
+    syntax.addFlag(kNameFlagShort, kNameFlagLong, OpenMaya.MSyntax.kString)
     syntax.addFlag(kIdFlagShort, kIdFlagLong, OpenMaya.MSyntax.kLong)
 
     return syntax
